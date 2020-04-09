77a12f7a41fcc0b2358afe38259a9a4afa878736
Luis Puente <puente.luism@gmail.com>
2020-04-09 15:29:52 -0500

    Add caesarsCipher

---
+++ src/arrayAnalisis.js
@@ -0,0 +1,28 @@
+const analyze = (array) => {
+  const arrLength = array.length;
+
+  const average = array.reduce((total, num) => total + num) / arrLength;
+ 
+  let max = array[0];
+  let min = array[0];
+
+  array.forEach((element) => {
+    if (element > max) {
+      max = element;
+    }
+    if (element < min) {
+      min = element;
+    }
+  });
+
+  const myObject = {
+    average,
+    min,
+    max,
+    length: arrLength,
+  };
+
+  return myObject;
+};
+
+export default analyze;
